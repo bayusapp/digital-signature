ecd699b3c276f9ae4f170eb1408132d0f4c5e935
Bayu Setya Ajie Perdana Putra <bayu.setya.ajie@gmail.com>
2020-10-18 23:05:23 +0700

    upload digital signature source code

---
+++ save.php
@@ -0,0 +1,9 @@
+<?php
+$result     = array();
+$imagedata  = base64_decode($_POST['img_data']);
+$filename   = md5(date("dmYhisA"));
+$file_name  = './signature/' . $filename . '.png';
+file_put_contents($file_name, $imagedata);
+$result['status']     = 1;
+$result['file_name']  = $file_name;
+echo json_encode($result);
